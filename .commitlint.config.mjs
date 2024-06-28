c0711d8c4821e801c14dd5f980830c9cdb95f655
Victor Payno <vpayno@users.noreply.github.com>
2024-06-27 20:29:43 -0700

    ci: add git commit workflow

---
+++ .commitlint.config.mjs
@@ -0,0 +1,177 @@
+export default {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ['@commitlint/config-conventional'],
+  /*
+   * Resolve and load conventional-changelog-atom from node_modules.
+   * Referenced packages must be installed
+   */
+  // parserPreset: 'conventional-changelog-atom',
+  /*
+   * Resolve and load @commitlint/format from node_modules.
+   * Referenced package must be installed
+   */
+  formatter: '@commitlint/format',
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 240],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 240],
+    'header-max-length': [2, 'always', 240],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'coral',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'release',
+        'revert',
+        'security',
+        'style',
+        'test',
+        'types',
+        'wip',
+        'workflow'
+      ],
+    ],
+  },
+  /*
+   * Array of functions that return true if commitlint should ignore the given message.
+   * Given array is merged with predefined functions, which consist of matchers like:
+   *
+   * - 'Merge pull request', 'Merge X into Y' or 'Merge branch X'
+   * - 'Revert X'
+   * - 'v1.2.3' (ie semver matcher)
+   * - 'Automatic merge X' or 'Auto-merged X into Y'
+   *
+   * To see full list, check https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/is-ignored/src/defaults.ts.
+   * To disable those ignores and run rules always, set `defaultIgnores: false` as shown below.
+   */
+  ignores: [(commit) => commit === ''],
+  /*
+   * Whether commitlint uses the default ignore rules, see the description above.
+   */
+  defaultIgnores: true,
+  /*
+   * Custom URL to show upon failure
+   */
+  helpUrl:
+    'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+  /*
+   * Custom prompt configs
+   */
+  prompt: {
+    messages: {},
+    questions: {
+      type: {
+        description: "Select the type of change that you're committing",
+        enum: {
+          feat: {
+            description: 'A new feature',
+            title: 'Features',
+            emoji: '✨',
+          },
+          fix: {
+            description: 'A bug fix',
+            title: 'Bug Fixes',
+            emoji: '🐛',
+          },
+          docs: {
+            description: 'Documentation only changes',
+            title: 'Documentation',
+            emoji: '📚',
+          },
+          style: {
+            description: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)',
+            title: 'Styles',
+            emoji: '💎',
+          },
+          refactor: {
+            description: 'A code change that neither fixes a bug nor adds a feature',
+            title: 'Code Refactoring',
+            emoji: '📦',
+          },
+          perf: {
+            description: 'A code change that improves performance',
+            title: 'Performance Improvements',
+            emoji: '🚀',
+          },
+          test: {
+            description: 'Adding missing tests or correcting existing tests',
+            title: 'Tests',
+            emoji: '🚨',
+          },
+          build: {
+            description: 'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)',
+            title: 'Builds',
+            emoji: '🛠',
+          },
+          ci: {
+            description:
+            'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)',
+            title: 'Continuous Integrations',
+            emoji: '⚙️',
+          },
+          chore: {
+            description: "Other changes that don't modify src or test files",
+            title: 'Chores',
+            emoji: '♻️',
+          },
+          revert: {
+            description: 'Reverts a previous commit',
+            title: 'Reverts',
+            emoji: '🗑',
+          },
+        },
+        scope: {
+          description: 'What is the scope of this change (e.g. component or file name)',
+        },
+        subject: {
+          description: 'Write a short, imperative tense description of the change',
+        },
+        body: {
+          description: 'Provide a longer description of the change',
+        },
+        isBreaking: {
+          description: 'Are there any breaking changes?',
+        },
+        breakingBody: {
+          description: 'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself',
+        },
+        breaking: {
+          description: 'Describe the breaking changes',
+        },
+        isIssueAffected: {
+          description: 'Does this change affect any open issues?',
+        },
+        issuesBody: {
+          description: 'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself',
+        },
+        issues: {
+          description: 'Add issue references (e.g. "fix #123", "re #123".)',
+        },
+      },
+    },
+  },
+};
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # rpi-pico-workspace
 
 [![actionlint](https://github.com/vpayno/rpi-pico-workspace/actions/workflows/gh-actions.yaml/badge.svg?branch=main)](https://github.com/vpayno/rpi-pico-workspace/actions/workflows/gh-actions.yaml)
+[![git](https://github.com/vpayno/rpi-pico-workspace/actions/workflows/git.yaml/badge.svg?branch=main)](https://github.com/vpayno/rpi-pico-workspace/actions/workflows/git.yaml)
 [![json](https://github.com/vpayno/rpi-pico-workspace/actions/workflows/json.yaml/badge.svg?branch=main)](https://github.com/vpayno/rpi-pico-workspace/actions/workflows/json.yaml)
 [![markdown](https://github.com/vpayno/rpi-pico-workspace/actions/workflows/markdown.yaml/badge.svg?branch=main)](https://github.com/vpayno/rpi-pico-workspace/actions/workflows/markdown.yaml)
 [![spellcheck](https://github.com/vpayno/rpi-pico-workspace/actions/workflows/spellcheck.yaml/badge.svg?branch=main)](https://github.com/vpayno/rpi-pico-workspace/actions/workflows/spellcheck.yaml)
